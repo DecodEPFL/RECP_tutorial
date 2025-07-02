7e0c6ccc800084e93cd5981ea3877037d39d2b5a
Mahrokh Ghoddousiboroujeni <72985932+MahrokhGB@users.noreply.github.com>
2025-07-02 08:48:55 +0200

    first commit

---
+++ write_to_file.py
@@ -0,0 +1,19 @@
+import argparse
+
+def main():
+    print('start')
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--text", default="hello world")
+    parser.add_argument("--output", default="hello.txt")
+
+    args = parser.parse_args()
+
+    print(f"{args.text}")
+
+    with open(args.output, "w") as f:
+        f.write(args.text)
+
+
+if __name__ == "__main__":
+    main()
